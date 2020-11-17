bfaffd46da6440e3a7d1ba156ce4af756f8e3644
galliarodgerLFP <74176429+galliarodgerLFP@users.noreply.github.com>
2020-11-17 11:40:25 +0200

    Updated to where I was at 11pm last night
    I don't know how to use Git that well so I hope this works - this is just the most recent version of the trial, i think there must be quite a few errors but will continue to update, sorry.

---
+++ UserFeaturesV2/Assets/Main.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Main : MonoBehaviour
+{
+    //Screens
+    public GameObject HomeScreen;
+    public GameObject RegisterScreen;
+    public GameObject LoginScreen;
+    public GameObject DetailsScreen;
+
+    //instances of class
+    public static Main instance;
+    public WebComm webcomm;
+    public UserInformation userInfo;
+
+    //for homepage
+    public Button registerUser;
+    public Button loginUser;
+
+    void Start()
+    {
+        instance = this;
+        SetHomeScreenActive();
+        webcomm = GetComponent<WebComm>();
+        userInfo = GetComponent<UserInformation>();
+
+        registerUser.onClick.AddListener(() =>
+        {
+            SetRegisterActive();
+        });
+        loginUser.onClick.AddListener(() =>
+        {
+            SetLoginActive();
+        });
+
+    }
+
+    #region views
+    public void SetHomeScreenActive()
+    {
+        HomeScreen.SetActive(true);
+        RegisterScreen.SetActive(false);
+        LoginScreen.SetActive(false);
+        DetailsScreen.SetActive(false);
+    }
+
+    public void SetRegisterActive()
+    {
+        HomeScreen.SetActive(false);
+        RegisterScreen.SetActive(true);
+        LoginScreen.SetActive(false);
+        DetailsScreen.SetActive(false);
+    }
+
+    public void SetLoginActive()
+    {
+        HomeScreen.SetActive(false);
+        RegisterScreen.SetActive(false);
+        LoginScreen.SetActive(true);
+        DetailsScreen.SetActive(false);
+    }
+
+    public void SetDetailActive()
+    {
+        HomeScreen.SetActive(false);
+        RegisterScreen.SetActive(false);
+        LoginScreen.SetActive(false);
+        DetailsScreen.SetActive(true);
+    }
+
+    #endregion
+
+}
---
+++ UserFeaturesV2/Assets/UserInformation.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserInformation : MonoBehaviour
+{
+    public string userID { get; private set; }//is dbID
+    public string userName, userPassword;
+    public int level;
+       
+    public string name, surname, address, email, cell, landline, maritialStatus;
+    public string race, gender, affiliate, idNum;
+
+    public void SetInfo(string username, string password)
+    {
+        userName = username;
+        userPassword = password;
+    }
+
+    public void SetAllInfo(string username, string password, int level, string name, string surname, string address, string gender, string email, string cell, string landline, string maritialStatus, string race, string affiliate, string idNum)
+    {
+        userName = username;
+        userPassword = password;
+        this.level = level;
+        this.name = name;
+        this.surname = surname;
+        this.address = address;
+        this.email = email;
+        this.cell = cell;
+        this.landline = landline;
+        this.maritialStatus = maritialStatus;
+        this.race = race;
+        this.gender = gender;
+        this.affiliate = affiliate;
+        this.idNum = idNum;
+    }
+    public void SetID(string id)
+    {
+        userID = id;
+    }
+
+    public void IncLevel(int currLevel)
+    {
+        if(level != 0)
+        {
+            level++;
+        }
+        else
+        {
+            level = 1;
+        }
+    }
+
+
+}
